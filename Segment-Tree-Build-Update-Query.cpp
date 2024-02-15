018e3ff63973bcb209f604f4e6daab49e4de7a44
Habibur Rahman <habibur.191522@gmail.com>
2024-02-15 16:00:36 +0600

    Update code for easy to understand

--- Segment-Tree-Build-Update-Query.cpp
+++ Segment-Tree-Build-Update-Query.cpp
@@ -1,4 +1,4 @@
-//Code never lies, LET'S START
+//Code never lies,
 //Author: Habib
 
 #include<bits/stdc++.h>
@@ -24,12 +24,58 @@ using namespace __gnu_pbds;
 #define  ed                cout<<"\n";
 
 const   ll    N = 2e5 + 5;
-const   ll     inf = 1e18;
 
 int test = 1 ;
-int seg[N<<2][3] , lazy[N<<2] ;		// In this initialy i take a 2D seg array and 1D lazy array to keep value
+int seg[N<<2][3] , lazy[N<<2] ;		                        // In this initialy i take a 2D seg array and 1D lazy array to keep value
 
-// Rotate the array one times
+void swap1( int &a , int &b , int &c ) ; 	                // Rotate this array one times
+void swap2( int &a , int &b , int &c ) ;	                // Rotate this array two times
+void propagate( int at , int low , int high ) ;             // Handle the lazy operation and rotate 
+void Build( int at , int low , int high ) ;			        // Building the array
+void Update( int at , int low , int high , int l , int r ) ;// Update the array in this given range L-R
+int Query( int at , int low , int high , int l , int r ) ;  // Caculate result between given range L-R
+void reset() ;	                                            // Reset the seg 2D array and lazy array because of the other test case operation
+
+
+void pipra(){
+	cout<< "Case " << (test++) << ":\n" ;
+    int n,q;	cin>>n>>q;
+
+    reset() ;
+    Build( 0 , 0 , n-1 ) ;
+
+    while( q-- ){
+    	int check,left,right;	cin>>check>>left>>right;
+    	if( check==0 ){
+    		Update( 0 , 0 , n-1 , left , right ) ;
+    	}
+    	else{
+    		int ans = Query( 0 , 0 , n-1 , left , right )  ;
+    		cout<< ans << endl ;
+    	}
+    }
+}
+     
+int main(){
+    //Code_by_Habib
+    FastIO;
+    int tt;    cin>>tt;
+    while(tt--)
+        pipra();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 void swap1( int &a , int &b , int &c ){
 	int z = a ;
 	int o = b ;
@@ -38,7 +84,6 @@ void swap1( int &a , int &b , int &c ){
 	b = z ;
 	c = o ;
 }
-// Rotate this array two times
 void swap2( int &a , int &b , int &c ){
 	int z = a ;
 	int o = b ;
@@ -48,7 +93,6 @@ void swap2( int &a , int &b , int &c ){
 	c = z ;
 }
 
-// Handle the lazy operation and rotate
 void propagate( int at , int low , int high ){
 	if( low != high ){		//  transfering the ith value in next child
 		lazy[2*at+1] = (lazy[at]+lazy[2*at+1]) % 3 ;
@@ -63,7 +107,6 @@ void propagate( int at , int low , int high ){
 	lazy[at] = 0 ;
 }
 
-// Building the array
 void Build( int at , int low , int high ){
 	if( low == high ){
 		seg[at][0] = 1 ;             // Initially its already divisible
@@ -78,7 +121,6 @@ void Build( int at , int low , int high ){
 	seg[at][1] = seg[at][2] = 0 ;
 }
 
-// Update the array in this given range L-R
 void Update( int at , int low , int high , int l , int r ){
 	if( lazy[at] )
 		propagate( at , low , high ) ;
@@ -117,7 +159,6 @@ int Query( int at , int low , int high , int l , int r ){
 		   Query( 2*at+2 , mid+1 , high , l , r ) ;
 }
 
-// Reset the seg 2D array and lazy array because of the other test case operation
 void reset(){
 	loop( i , 0 , 4*N ){
 		seg[i][0] = seg[i][1] = seg[i][2] = 0 ;
@@ -125,30 +166,3 @@ void reset(){
 	}
 }
 
-void pipra(){
-	cout<< "Case " << (test++) << ":\n" ;
-    int n,q;	cin>>n>>q;
-
-    reset() ;
-    Build( 0 , 0 , n-1 ) ;
-
-    while( q-- ){
-    	int check,left,right;	cin>>check>>left>>right;
-    	if( check==0 ){
-    		Update( 0 , 0 , n-1 , left , right ) ;
-    	}
-    	else{
-    		int ans = Query( 0 , 0 , n-1 , left , right )  ;
-    		cout<< ans << endl ;
-    	}
-    }
-}
-     
-int main(){
-    //Code_by_Habib
-    FastIO;
-    int tt;    cin>>tt;
-    while(tt--)
-        pipra();
-    return 0;
-}
